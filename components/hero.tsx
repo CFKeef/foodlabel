e6ace0dfa93f8c4a0416dd9b50d18ca5709f9d5c
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2021-01-12 16:12:39 -0500

    Hero done for desktop and mobile

---
+++ components/hero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../styles/Hero.module.scss";
+import HeroImage from "../public/images/HeroImage.svg";
+import Link from "next/link";
+
+const Hero: React.FunctionComponent = () => {
+  return (
+    <section className={styles.heroContainer}>
+      <div className={styles.textContainer}>
+        <h1>Generate FDA Approved Nutrition Labels In Minutes</h1>
+        <p>
+          We allow you to create FDA compliant nutrition fact labels in the
+          simplest way possible
+        </p>
+        <Link href={"/demo"}>Watch a Demo</Link>
+      </div>
+      <div className={styles.imageContainer}>
+        <img
+          src={HeroImage}
+          alt={"Decorative image depicting nutrition labels"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Layout from "../components/layout";
+import Hero from "../components/hero";
 
 function Home() {
   return (
@@ -8,7 +9,7 @@ function Home() {
       <Head>
         <title>food label</title> <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section>asdsadasdasdasd</section>
+      <Hero />
     </Layout>
   );
 }
