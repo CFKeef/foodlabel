e2868dd516207fc37345077e7e253639ff3f4907
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2021-01-19 16:37:45 -0500

    Started Dash

--- components/dash.tsx
+++ components/dash.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import styles from "../styles/Dashboard.module.scss";
 const Dash: React.FunctionComponent = () => {
-  return <div>dash</div>;
+  return (
+    <section className={styles.dashContainer}>
+      <div className={styles.menu}>
+        <div className={styles.leftContainer}>
+          <h2>Created Labels</h2>
+          <input placeholder="Search" type="text"></input>
+          <div></div>
+        </div>
+        <div className={styles.rightContainer}>
+          <button>Create New Label</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dash;
