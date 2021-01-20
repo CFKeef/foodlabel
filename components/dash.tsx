06c2348debdf64645782b1ee3176bcd1b48f980e
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2021-01-20 15:19:42 -0500

    Empty DAsh done

--- components/dash.tsx
+++ components/dash.tsx
@@ -1,12 +1,55 @@
 import React from "react";
-import styles from "../styles/Dashboard.module.scss";
+import styles from "../styles/Dash.module.scss";
 import { Label } from "../types";
+import Clone from "../public/images/clone.svg";
+import Delete from "../public/images/delete.svg";
+import Download from "../public/images/Download.svg";
+import Edit from "../public/images/edit.svg";
 
 interface dashProps {
   labels: Label[];
 }
 
-const Dash: React.FunctionComponent<dashProps> = (props) => {
+const Dash: React.FunctionComponent<dashProps> = ({ labels }) => {
+  const generateTableRows = () => {
+    if (labels.length === 0) {
+      return (
+        <tbody>
+          <tr className={styles.emptyTable}>
+            <td colSpan={4}>Create a label to populate this table!</td>
+          </tr>
+        </tbody>
+      );
+    }
+
+    return (
+      <tbody>
+        {labels.map((label) => {
+          return (
+            <tr>
+              <td>{label.name}</td>
+              <td>{label.date}</td>
+              <td>{label.status}</td>
+              <td>
+                <button>
+                  <img src={Edit} />
+                </button>
+                <button>
+                  <img src={Delete} />
+                </button>
+                <button>
+                  <img src={Download} />
+                </button>
+                <button>
+                  <img src={Clone} />
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  };
   return (
     <section className={styles.dashContainer}>
       <div className={styles.menu}>
@@ -19,17 +62,24 @@ const Dash: React.FunctionComponent<dashProps> = (props) => {
           <button>Create New Label</button>
         </div>
       </div>
-      <table>
-        <caption>Label History</caption>
+      <table className={styles.table}>
         <thead>
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Date</th>
-            <th scope="col">Status</th>
-            <th scope="col">Actions</th>
+            <th className={styles.nameRow} scope="col">
+              Name
+            </th>
+            <th className={styles.dateRow} scope="col">
+              Date
+            </th>
+            <th className={styles.statusRow} scope="col">
+              Status
+            </th>
+            <th className={styles.actionsRow} scope="col">
+              Actions
+            </th>
           </tr>
         </thead>
-        <tbody></tbody>
+        {generateTableRows()}
       </table>
     </section>
   );
